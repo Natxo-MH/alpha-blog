1bd4b0838221e75394e98a3cb681f8d692201f79
Rails Course <natxo_mh@yahoo.es>
2017-06-12 13:36:56 +0200

    Crontrol de errores y Show template

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,17 +6,31 @@ class ArticlesController < ApplicationController
 
   end
 
+
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article was succesfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
   end
 
 
-private
 
-  def article_params
-    params.require(:article).permit(:title, :description)
+  def show
+    @article = Article.find(params[:id])
   end
+
+
+
+  private
+
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+
+
 
 end
