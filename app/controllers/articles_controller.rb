ceb5fe602f4e2108fd1a59782ac692e87090f7f6
Rails Course <natxo_mh@yahoo.es>
2017-06-17 14:08:57 +0200

    Style errors and flash messeges

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,7 +21,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was succesfully created"
+      flash[:success] = "Article was succesfully created"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -32,7 +32,7 @@ class ArticlesController < ApplicationController
   def update
         
     if @article.update(article_params)
-      flash[:notice] = "Article vas succesfully updated"
+      flash[:success] = "Article vas succesfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -49,7 +49,7 @@ class ArticlesController < ApplicationController
       
     @article.destroy
 
-    flash[:notice] = "Article was successfully deleted"
+    flash[:danger] = "Article was successfully deleted"
 
     redirect_to articles_path
 
